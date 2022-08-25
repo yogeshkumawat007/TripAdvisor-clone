30df703ed29535f7d90c417dc48d9eca9618df0c
yogeshkumawat007 <93376662+yogeshkumawat007@users.noreply.github.com>
2022-08-25 16:57:25 +0530

    day-2

---
+++ script_yogi/navbar2_yogi.js
@@ -0,0 +1,11 @@
+function searchCityName(){
+    console.log('here');
+}
+
+function hotels(){
+    console.log("add path to hotels page")
+}
+
+function hotels(){
+    console.log("add path to things to do page")
+}
