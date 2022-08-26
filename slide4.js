9209b152714f3165108222eca0058b7ed3b2d208
sagar palya <sagar.palya@gmail.com>
2022-08-26 16:27:54 +0530

    Add File

---
+++ slide4.js
@@ -0,0 +1,16 @@
+const productContainers = [...document.querySelectorAll('.product1-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn1')];
+const preBtn = [...document.querySelectorAll('.pre-btn1')];
+
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
